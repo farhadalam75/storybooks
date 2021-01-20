40fe83c084b57231184984f8d762b240b7dd2791
Farhad <farhad.game@orion-group.net>
2021-01-20 17:12:55 +0600

    morgan logging express-handlebars // views routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 
 //Load config
 dotenv.config({path: './config/config.env'})
@@ -8,6 +10,18 @@ dotenv.config({path: './config/config.env'})
 connectDB()
 
 const app = express()
+
+//Logging
+if (process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
