ce68cd61cd372fca310801d7fbf2dc037eb747cb
Farhad <farhad.game@orion-group.net>
2021-01-20 18:04:45 +0600

    static folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
@@ -21,6 +22,10 @@ if (process.env.NODE_ENV === 'development')
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Routes
 app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
