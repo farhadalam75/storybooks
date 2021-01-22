f0c5c1d960991a2816c865e1b6ff19fced405fce
farhadalam75 <farhadalam75@gmail.com>
2021-01-22 16:29:10 +0600

    google+ API google authentication passport auth session model profile authentication page callback to ?

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
-const connectDB = require('./config/db')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
 
-//Load config
+// Load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
 const app = express()
 
-//Logging
+// Logging
 if (process.env.NODE_ENV === 'development')
 {
     app.use(morgan('dev'))
 }
 
-//Handlebars
+// Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-//Static folder
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-//Routes
+// Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
